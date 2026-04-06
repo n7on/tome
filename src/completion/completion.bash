99de1d2953c4db9676e5272f2db26dc4cb5aa491
Anton Lindström <lindstrom.anton@gmail.com>
2026-04-06 21:50:52 +0200

    zsh completion added

--- src/completion/completion.bash
+++ src/completion/completion.bash
@@ -3,7 +3,14 @@
 completion_bash() {
     _grim_command_param_parse "$@" || return 1
 
-    _grim_command_exec_python completion completion.py "$_GRIM_DIR/src"
+    _grim_command_exec_python completion completion.py "$_GRIM_DIR/src" --shell bash
+}
+
+completion_zsh() {
+    _grim_command_param_parse "$@" || return 1
+
+    _grim_command_exec_python completion completion.py "$_GRIM_DIR/src" --shell zsh
 }
 
 _grim_command_complete_params "completion_bash" "Generate bash completion script for the grim binary"
+_grim_command_complete_params "completion_zsh" "Generate zsh completion script for the grim binary"
